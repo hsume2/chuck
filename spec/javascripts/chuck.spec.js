58ad95976491e84179d5f1c686275fcfe72052ad
Henry Hsu <hhsu@zendesk.com>
2012-04-04 13:13:30 -0700

    Full coverage

--- spec/javascripts/chuck.spec.js
+++ spec/javascripts/chuck.spec.js
@@ -115,20 +115,40 @@ describe('Chuck', function(){
 
   });
 
-  if(typeof window !== 'undefined') {
+  if(typeof window === 'undefined') {
+
+    describe('Node', function() {
+
+      describe('#adapter', function(){
+
+        it('should have adapter', function() {
+          var sut = chuck('testing');
+          assert.isObject(sut.adapter);
+        });
+
+      });
+
+    });
+
+  }
+
+  describe('Browser', function() {
 
     describe('#adapter', function(){
 
-      it('should have adapter', function() {
-        var sut = chuck('testing');
-        assert.isObject(sut.adapter);
-        assert.equal(sut.adapter.jQuery, jQuery);
-      });
+      it('should create chuck with browser adapter', function() {
+        if(typeof window === 'undefined') {
+          window = sinon.stub();
+          window.isStub = true;
+        }
 
-      it('should create chuck with options', function() {
         var fakeQuery = sinon.stub();
         var sut = chuck('testing', { jQuery: fakeQuery });
         assert.equal(sut.adapter.jQuery, fakeQuery);
+
+        if(typeof window !== 'undefined' && window.isStub) {
+          delete window;
+        }
       });
 
     });
@@ -136,27 +156,16 @@ describe('Chuck', function(){
     describe('#send()', function(){
 
       it('should POST to API end-point', function() {
-        var sut = chuck('testing');
+        if(typeof window === 'undefined') {
+          window = sinon.stub();
+          window.isStub = true;
+        }
 
-        sut.log('a', { event: '1' });
-        sut.log('b', { event: '2' });
-
-        this.clock.tick(sut.timeout);
-
-        assert.length(this.sandbox.server.requests, 1);
-
-        var request = this.sandbox.server.requests[0];
-
-        assert.equal(request.url, '/chuck');
-        assert.equal(request.method, 'POST');
-        assert.equal(request.async, true);
-
-        assert.equal(request.requestHeaders['Content-Type'], 'application/json;charset=utf-8');
-        assert.equal(request.requestHeaders['Accept'], 'application/json, text/javascript, */*; q=0.01');
-        assert.equal(request.requestHeaders['X-Requested-With'], 'XMLHttpRequest');
-
-        assert.equal(request.requestBody, JSON.stringify({
-          payload: [
+        var fakeQuery = { ajax: function() {} };
+        var expectation = sinon.mock(fakeQuery).expects('ajax').withArgs({
+          contentType: "application/json",
+          data: JSON.stringify({
+            payload: [
             {
               scope: 'a',
               properties: {
@@ -169,19 +178,84 @@ describe('Chuck', function(){
                 event: '2'
               }
             }
-          ]
-        }));
+            ]
+          }),
+          dataType: "json",
+          type: "POST",
+          url: "/chuck"
+        }).once();
+        var sut = chuck('testing', { jQuery: fakeQuery });
+
+        sut.log('a', { event: '1' });
+        sut.log('b', { event: '2' });
+
+        this.clock.tick(sut.timeout);
+
+        expectation.verify();
+
+        if(typeof window !== 'undefined' && window.isStub) {
+          delete window;
+        }
       });
 
     });
 
-  } else {
+  });
 
-    describe('#adapter', function(){
+  if(typeof window !== 'undefined') {
 
-      it('should have adapter', function() {
-        var sut = chuck('testing');
-        assert.isObject(sut.adapter);
+    describe('Live Browser', function() {
+
+      describe('#adapter', function(){
+
+        it('should have adapter with window.jQuery', function() {
+          var sut = chuck('testing');
+          assert.isObject(sut.adapter);
+          assert.equal(sut.adapter.jQuery, jQuery);
+        });
+
+      });
+
+      describe('#send()', function(){
+
+        it('should POST to API end-point', function() {
+          var sut = chuck('testing');
+
+          sut.log('a', { event: '1' });
+          sut.log('b', { event: '2' });
+
+          this.clock.tick(sut.timeout);
+
+          assert.length(this.sandbox.server.requests, 1);
+
+          var request = this.sandbox.server.requests[0];
+
+          assert.equal(request.url, '/chuck');
+          assert.equal(request.method, 'POST');
+          assert.equal(request.async, true);
+
+          assert.equal(request.requestHeaders['Content-Type'], 'application/json;charset=utf-8');
+          assert.equal(request.requestHeaders['Accept'], 'application/json, text/javascript, */*; q=0.01');
+          assert.equal(request.requestHeaders['X-Requested-With'], 'XMLHttpRequest');
+
+          assert.equal(request.requestBody, JSON.stringify({
+            payload: [
+              {
+                scope: 'a',
+                properties: {
+                  event: '1'
+                }
+              },
+              {
+                scope: 'b',
+                properties: {
+                  event: '2'
+                }
+              }
+            ]
+          }));
+        });
+
       });
 
     });
